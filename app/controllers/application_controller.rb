16c0556d207b2862ab6df0feef350a7908f01dbd
Athena Saldanha <asaldanha@chicagobooth.edu>
2023-04-06 19:36:46 +0000

    streamline code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,14 @@ class ApplicationController < ActionController::Base
   def play_rock
     #write a ton of code
     #render({:html=> "<h1>owdy World!</h1>".html_safe})
+    @comp_move= ["rock","paper", "scissors"].sample
+    if @comp_move == "rock"
+      @outcome="We tied!"
+    elsif @comp_move == "paper"
+      @outcome="We lost!"
+    elsif @comp_move == "scissors"
+      @outcome="We won!"
+    end
     render({:template=>"game_templates/user_rock.html.erb"})
     #redirect_to("https://www.wikipedia.org")
   end
@@ -19,6 +27,15 @@ class ApplicationController < ActionController::Base
   def play_paper
     #write a ton of code
     #render({:html=> "<h1>owdy World!</h1>".html_safe})
+    
+    @comp_move= ["rock","paper", "scissors"].sample
+    if @comp_move == "rock"
+      @outcome="We won!"
+    elsif @comp_move == "paper"
+      @outcome="We tied!"
+    elsif @comp_move == "scissors"
+      @outcome="We lost!"
+    end
     render({:template=>"game_templates/user_paper.html.erb"})
     #redirect_to("https://www.wikipedia.org")
   end
@@ -26,6 +43,14 @@ class ApplicationController < ActionController::Base
   def play_scissors
     #write a ton of code
     #render({:html=> "<h1>owdy World!</h1>".html_safe})
+    @comp_move= ["rock","paper", "scissors"].sample
+    if @comp_move == "rock"
+      @outcome="We lost!"
+    elsif @comp_move == "paper"
+      @outcome="We won!"
+    elsif @comp_move == "scissors"
+      @outcome="We tied!"
+    end
     render({:template=>"game_templates/user_scissors.html.erb"})
     #redirect_to("https://www.wikipedia.org")
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
 
   get("/paper",{:controller=>"application",:action =>"play_paper"})
 
-  #get("/scissors",{:controller=>"application",:action =>"play_scissors"})
+  get("/scissors",{:controller=>"application",:action =>"play_scissors"})
 end
