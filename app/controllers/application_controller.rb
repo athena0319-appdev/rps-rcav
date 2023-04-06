6a20556f38c595833d82b64c845acac0f3f4b459
Athena Saldanha <asaldanha@chicagobooth.edu>
2023-04-06 19:06:13 +0000

    Rock routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def play_rock
+    #write a ton of code
+    #render({:html=> "<h1>owdy World!</h1>".html_safe})
+    render({:template=>"game_templates/user_rock.html.erb"})
+    #redirect_to("https://www.wikipedia.org")
+
+  end
 end
